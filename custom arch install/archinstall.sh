0e8250101ec07920f8661aea5953aab32bee2c95
Trinteen <140308014+trinteen@users.noreply.github.com>
2023-09-21 16:56:13 +0200

    Update archinstall.sh

--- custom arch install/archinstall.sh
+++ custom arch install/archinstall.sh
@@ -40,7 +40,7 @@ export V_USER_PASS="user"
 export V_GUI="xfce4 lightdm lightdm-gtk-greeter"
 
 #=> Defines extra packages:
-export V_EXTRA_PKG="zip unzip unrar plank xarchiver thunar-archive-plugin thunar-shares-plugin thunar-media-tags-plugin"
+export V_EXTRA_PKG="zip unzip unrar plank xarchiver thunar-archive-plugin thunar-shares-plugin thunar-media-tags-plugin rofi"
 
 #=> Defines AUR packages (NOT WORKING NOW):
 export V_AUR_PKG=("zentile-bin")
