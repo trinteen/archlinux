db692c7f2b6c3b576fa26b0779463e4d9123592a
trinteen <140308014+trinteen@users.noreply.github.com>
2025-03-18 16:58:09 +0100

    Update archinstall.sh

--- custom arch install/archinstall.sh
+++ custom arch install/archinstall.sh
@@ -279,7 +279,7 @@ echo "=> 6. Post-install chroot settings"
 
     #=> Install AUR packages:
     echo ":: Install AUR packages"
-    arch-chroot /mnt bash -c "echo 'root ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/root" 1> /dev/null
+    arch-chroot /mnt bash -c "echo -e 'root ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/root" 1> /dev/null
     for aur in ${V_AUR_PKG[@]}; do
         echo ${aur}
         arch-chroot -u ${V_USER_NAME} /mnt bash -c "paru --sudoloop --noconfirm --needed -S ${aur}" 1> /dev/null
